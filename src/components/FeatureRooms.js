18bdb396ebb705d8bf401ed62505ecec5021e734
muath93 <mu3athme@gmail.com>
2019-07-24 16:37:56 +0300

    added loading component

--- src/components/FeatureRooms.js
+++ src/components/FeatureRooms.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../context/Context';
+import Title from './Title';
+import Loading from './Loading';
+import Room from './Room';
+
 class FeatureRooms extends Component {
   static contextType = RoomContext;
 
   render() {
-    const { featuredRooms } = this.context;
-    console.log(featuredRooms);
+    let { loading, featuredRooms: rooms } = this.context;
+    console.log(rooms);
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
 
-    return <div>feature rooms </div>;
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
 
