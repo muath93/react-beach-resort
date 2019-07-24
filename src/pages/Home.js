c3b164fc446613a55fcce60a7e24e36bef9069ab
muath93 <mu3athme@gmail.com>
2019-07-24 11:27:54 +0300

    added Services component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,16 +2,20 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="luxurios rooms" subtitle="deluxe rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="luxurios rooms" subtitle="deluxe rooms starting at $299">
+          <Link to="/rooms" className="btn-primary">
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
