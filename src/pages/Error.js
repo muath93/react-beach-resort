25323293caf21657042f5f3a2a7b05fe0cd39620
muath93 <mu3athme@gmail.com>
2019-07-24 08:46:19 +0300

    first commit

---
+++ src/pages/Error.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+  render() {
+    return <div>hello from Error page</div>;
+  }
+}
+
+export default Error;
---
+++ src/pages/SingleRoom.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class SingleRoom extends Component {
+  render() {
+    return <div>hello from Single Room page</div>;
+  }
+}
+
+export default SingleRoom;
