24ddc55727f49508070d375cb2c01831a0c7b234
muath93 <mu3athme@gmail.com>
2019-07-28 17:19:44 +0300

    added filter by size

--- src/components/RoomsFilter.js
+++ src/components/RoomsFilter.js
@@ -89,7 +89,54 @@ const RoomsFilter = ({ rooms }) => {
             className="form-control"
           />
         </div>
-        {/* end ofroom price */}
+        {/* end of room price */}
+        {/* size */}
+        <div className="form-group">
+          <label htmlFor="size">size</label>
+          <div className="size-inputs">
+            <input
+              type="number"
+              name="minSize"
+              id="size"
+              value={minSize}
+              onChange={handelChange}
+              className="size-input"
+            />
+            <input
+              type="number"
+              name="maxSize"
+              id="size"
+              value={maxSize}
+              onChange={handelChange}
+              className="size-input"
+            />
+          </div>
+        </div>
+        {/* end of size */}
+        {/* extras */}
+        <div className="form-group">
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="breakfast"
+              id="breakfast"
+              checked={breakfast}
+              onChange={handelChange}
+            />
+            <label htmlFor="breakfast">breakfast</label>
+          </div>
+          <div className="single-extra">
+            <input
+              type="checkbox"
+              name="pets"
+              id="pets"
+              checked={pets}
+              onChange={handelChange}
+            />
+            <label htmlFor="pets">pets</label>
+          </div>
+        </div>
+        {/* end of extras */}
       </form>
     </section>
   );
--- src/context/Context.js
+++ src/context/Context.js
@@ -91,6 +91,11 @@ class RoomProvider extends Component {
     // filter by price
     tempRooms = tempRooms.filter(room => room.price <= price);
 
+    // filter by size
+    tempRooms = tempRooms.filter(
+      room => room.size >= minSize && room.size <= maxSize
+    );
+
     // change state
     this.setState({ sortedRooms: tempRooms });
   };
